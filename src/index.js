076902e77a1264d401b5c8d257cc29029e3e1696
Henry Lin <henry.lin@neo4j.com>
2023-10-06 20:59:56 -0400

    poc

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import {CypherParser, CypherLexer} from './lang/index.js'
+import antlr4 from 'antlr4'
+
+class MyErrorListener extends antlr4.error.ErrorListener {
+  errors = [];
+
+  // eslint-disable-next-line no-unused-vars
+  syntaxError(rec, sym, line, col, msg, e) {
+    const { start, stop } = sym || {};
+    console.log('msg', msg)
+    if (msg === "mismatched input '<EOF>' expecting {';', SP}") {
+      // suppress error about missing semicolon at the end of a query
+      return;
+    }
+    if (msg === "missing ';' at '<EOF>'") {
+      return;
+    }
+    if (
+      msg ===
+      "mismatched input '<EOF>' expecting {':', CYPHER, EXPLAIN, PROFILE, USING, CREATE, DROP, LOAD, WITH, OPTIONAL, MATCH, UNWIND, MERGE, SET, DETACH, DELETE, REMOVE, FOREACH, RETURN, START, CALL}"
+    ) {
+      return;
+    }
+    this.errors.push({ line, col, msg, start, stop });
+  }
+}
+
+const qry = 'match (n) return n,';
+console.log(qry)
+const errorListener = new MyErrorListener();
+const chars = new antlr4.InputStream(qry);
+const lexer = new CypherLexer(chars)
+lexer.removeErrorListeners();
+lexer.addErrorListener(errorListener);
+
+const tokens = new antlr4.CommonTokenStream(lexer);
+const parser = new CypherParser(tokens);
+parser.buildParseTrees = true;
+
+parser.removeErrorListeners();
+parser.addErrorListener(errorListener);
+const parseTree = parser.cypher()
+
+console.log('\n\n\n');
+for (let error of errorListener.errors) {
+    console.log(error.msg);
+    console.log('\n')
+}
---
+++ src/lang/index.js
@@ -0,0 +1,5 @@
+import CypherLexer from "./CypherLexer.js";
+import CypherListener from "./CypherListener.js";
+import CypherParser from "./CypherParser.js";
+
+export { CypherLexer, CypherListener, CypherParser };
